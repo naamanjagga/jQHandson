9282f0a6b26d84b85d703203e0e8223d63787892
naamanjagga <namanjagga@cedcommerce.com>
2022-02-11 17:05:51 +0530

    Use the correct selector to hide all odd table rows in a table.

--- src/script.js
+++ src/script.js
@@ -8,4 +8,6 @@ $(document).ready(function() {
     $("*").hide();  //Use the correct selector to hide all elements in the document.
 
     $("[href]").hide();  //Use the correct selector to hide all elements with an href attribute.
+
+    $("tr:odd").hide();  //Use the correct selector to hide all odd table rows in a table.
 });
