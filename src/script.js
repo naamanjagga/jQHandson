4a3c24dd318212b3ac812c26f69df1bfdc9a0fe7
naamanjagga <namanjagga@cedcommerce.com>
2022-02-11 16:58:50 +0530

    Use the correct selector to hide all elements in the document.

--- src/script.js
+++ src/script.js
@@ -4,4 +4,8 @@ $(document).ready(function() {
 	$('#test').hide();  //Use the correct selector to hide an element with id="test".
 
     $(".test").hide();  //Use the correct selector to hide all elements with class="test"
+
+    $("*").hide();  //Use the correct selector to hide all elements in the document.
+
+
 });
