932c4c121a878c2ab47d04704fa3a43fcf4e2526
naamanjagga <namanjagga@cedcommerce.com>
2022-02-11 18:11:26 +0530

    Use a jQuery method to stop the animation effect of a <div> element.

--- src/script.js
+++ src/script.js
@@ -83,6 +83,8 @@ $(document).ready(function() {
 
     $("div").animate({fontSize: '100px'}, "slow"); //Use the animate() method to set the font-size of a <div> element to 100 pixels:
 
+    $("div").stop();      //Use a jQuery method to stop the animation effect of a <div> element.
+
 
 
 
