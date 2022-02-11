2d857158ccb098d049dec4758ac8876effc1f10f
naamanjagga <namanjagga@cedcommerce.com>
2022-02-11 17:08:50 +0530

    Use the correct event to hide all <p> elements with a click.

--- src/script.js
+++ src/script.js
@@ -10,4 +10,10 @@ $(document).ready(function() {
     $("[href]").hide();  //Use the correct selector to hide all elements with an href attribute.
 
     $("tr:odd").hide();  //Use the correct selector to hide all odd table rows in a table.
+
+    $("p").click(function(){
+      $(this).hide();
+    });                        //Use the correct event to hide all <p> elements with a "click".
+
+
 });
