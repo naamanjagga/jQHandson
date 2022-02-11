fd78395e2a86c8d5868edb1a1610a6c47328109f
naamanjagga <namanjagga@cedcommerce.com>
2022-02-11 16:57:30 +0530

    Use the correct selector to hide an element with class=test.

--- src/script.js
+++ src/script.js
@@ -2,4 +2,6 @@ $(document).ready(function() {
 	$('p').hide();
 
 	$('#test').hide();  //Use the correct selector to hide an element with id="test".
+
+    $(".test").hide();  //Use the correct selector to hide all elements with class="test"
 });
